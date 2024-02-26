729c223a50c5d0bc4f35c25f3021c7ca3fa3a844
allllfo <mirae4292@gmail.com>
2024-02-27 08:59:12 +0900

    Fix: userObj null 체크 수정

--- frontend/src/routes/MainPage.jsx
+++ frontend/src/routes/MainPage.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import CodeShare from "./codeShare/CodeShare";
 import SetLink from "./codeShare/SetLink";
 import Question from "./question/Question";
 import { useDispatch, useSelector } from "react-redux";
+import { redirect, useNavigate, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function MainPage() {
   const [codelink, setCodelink] = useState("");
   const [showCodeShare, setShowCodeShare] = useState(false);
   const [showLinkInput, setshowLinkInput] = useState(false);
-
   const [showQuestion, setShowQuestion] = useState(false);
+  const navigate = useNavigate();
 
   const userObj = useSelector((state) => {
     return state.user.userInfo;
   });
+
+  useEffect(() => {
+    console.log("hello");
+    if (userObj === null) {
+      alert("로그인이 필요합니다.");
+      // 로그인 페이지로 이동
+      navigate("/users/login");
+    }
+  }, [userObj, history]);
+
   const handleShowCodeShare = () => {
     if (!codelink) {
       alert("화면 공유 링크를 먼저 삽입해주세요");
@@ -35,7 +46,7 @@ export default function MainPage() {
   return (
     <div>
       <div style={{ display: "flex", gap: 50 }}>
-        {userObj.admin === 0 ? (
+        {userObj && userObj.admin === 0 ? (
           <>
             <Button onClick={handleShowQuestion}>🙋‍♀️ 질문하기</Button>
             <Button onClick={handleShowLinkInput}>🖥️ 화면공유 하기</Button>
