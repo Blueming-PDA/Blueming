547eb9fae15bdbfa23c338b67776a0594493956f
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-29 02:49:35 +0900

    Release: socket 오류 수정

--- backend/utils/socket/socket.js
+++ backend/utils/socket/socket.js
@@ -1,12 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server({
-  cors: {
-    // origin: "http://127.0.0.1:5173",
-    origin: "http://3.38.84.174:5173",
-
-  },
-});
+const io = new Server();
 
 require("./socketEvents")(io);
 
--- frontend/src/routes/socket/socket.js
+++ frontend/src/routes/socket/socket.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
-
 // 서버의 포트를 통해 io를 받아온다
 // const socket = io("http://127.0.0.1:3000");
-const socket = io("http://3.38.84.174:3000");
-
+const socket = io("");
 
 export default socket;
