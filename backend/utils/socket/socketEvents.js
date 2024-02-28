8a24e5e8c37fa1613572d9000a19ea0e0c3ce11f
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-02-29 01:14:41 +0900

    Fix: 헤더 소켓 기능 수정

--- backend/utils/socket/socketEvents.js
+++ backend/utils/socket/socketEvents.js
@@ -1,24 +1,35 @@
 const User = require("../../models/User");
 const Alarm = require("../../models/Alarm");
+const Message = require("../../models/Message");
 module.exports = function (io) {
   io.on("connection", async (socket) => {
     console.log("a user connected", socket.id);
     console.log("===============");
 
     socket.on("setSid", async (userObj) => {
-      console.log("SocketID::::", socket.id);
+      // console.log("SocketID::::", socket.id);
       const user = await User.setSid(userObj.email, socket.id);
-      console.log("User.setSid를 보내고 받아온 USER !!!!!!!", user);
+      // console.log("User.setSid를 보내고 받아온 USER !!!!!!!", user);
     });
 
+    socket.on("setHeaderMessage", async ({ userObj, newMessage }) => {
+      const user = await User.setSid(userObj.email, socket.id);
+      console.log("User.setSid를 보내고 받아온 USER2 !!!!!!!", user);
 
-    socket.on("setHeaderMessage", async (message) => {
-      console.log("message :: ", message);
-
-      await socket.emit("setHeaderMessageBack", message);
+      await Message.create({
+        userId: user._id,
+        userName: user.name,
+        messageContent: newMessage,
+      });
     });
 
-    
+    socket.on("getRecentMessage", async () => {
+      const recentMessage = await Message.findOne().sort({
+        messageCreatedAt: -1,
+      });
+      socket.emit("setHeaderMessageBack", recentMessage.messageContent);
+    });
+
     socket.on("disconnect", () => {
       console.log("user disconnected");
     });
